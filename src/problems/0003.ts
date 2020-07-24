41a9cac9c5068381803213fa9f66c6930ced944f
Mauricio Ludwig <mrdamiengallagher@gmail.com>
2020-07-24 06:29:49 +0200

    0003

---
+++ src/problems/0003.ts
@@ -0,0 +1,32 @@
+import { success, end } from '../utils/logger';
+
+export default () => {
+  let num = 600851475143;
+  const primeNumbers: number[] = [];
+  let loop = true;
+
+  while (loop) {
+    let divide = 2;
+
+    while (true) {
+      if (num === divide) {
+        loop = false;
+        break;
+      }
+
+      if (num % divide === 0) {
+        num /= divide;
+        break;
+      }
+
+      divide++;
+    }
+
+    primeNumbers.push(divide);
+  }
+
+  const [largestFactor] = primeNumbers.sort((a, b) => b - a);
+
+  success(`Answer: ${largestFactor}`);
+  end();
+};
