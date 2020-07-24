7f185b4b4a2a4b69324750c8baa41fc7be4e26bf
Mauricio Ludwig <mrdamiengallagher@gmail.com>
2020-07-24 05:34:15 +0200

    0001

---
+++ src/problems/0001.ts
@@ -0,0 +1,15 @@
+import { success, end } from '../utils/logger';
+
+export default () => {
+  const maxNum = 1000;
+  let sum = 0;
+
+  for (let i = 1; i < maxNum; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
+    }
+  }
+
+  success(`Sum: ${sum}`);
+  end();
+};
