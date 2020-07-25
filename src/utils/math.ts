844d79bbccf332c99e3c28b965ea006dfe6c5a78
Mauricio Ludwig <mrdamiengallagher@gmail.com>
2020-07-25 04:28:47 +0200

    math function

---
+++ src/utils/math.ts
@@ -0,0 +1,19 @@
+export const isPrimeNumber = (n: number, primeNumbers: number[]) => {
+  const isDivisibleByTwo = n % 2 === 0;
+  const isDivisibleByFive = n % 5 === 0;
+  const isDivisibleByPrimeNumbers = primeNumbers.some((o) => n % o === 0);
+
+  const sumOfDigits = n
+    .toString()
+    .split('')
+    .map(Number)
+    .reduce((acc, curr) => acc + curr, 0);
+  const isDivisibleByThree = sumOfDigits % 3 === 0;
+
+  return !(
+    isDivisibleByTwo ||
+    isDivisibleByThree ||
+    isDivisibleByFive ||
+    isDivisibleByPrimeNumbers
+  );
+};
