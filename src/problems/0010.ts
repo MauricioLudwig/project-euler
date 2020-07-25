a239488708559f080c30f077ba4b2f1da75eb348
Mauricio Ludwig <mrdamiengallagher@gmail.com>
2020-07-25 15:12:43 +0200

    refactor

--- src/problems/0010.ts
+++ src/problems/0010.ts
@@ -1,9 +1,12 @@
-import { success, end } from '../utils/logger';
+import { performance } from 'perf_hooks';
+import { success, end, logPerformance } from '../utils/logger';
 import { isPrimeNumber } from '../utils/math';
 
 export default () => {
   const primeNumbers = [2, 3, 5];
   let n = 6;
+
+  const t1 = performance.now();
 
   while (n <= 2000000) {
     if (isPrimeNumber(n, primeNumbers)) {
@@ -12,6 +15,10 @@ export default () => {
 
     n++;
   }
+
+  const t2 = performance.now();
+
+  logPerformance(t2, t1);
 
   const sum = primeNumbers.reduce((acc, curr) => acc + curr, 0);
   success(`Answer: ${sum}`);
