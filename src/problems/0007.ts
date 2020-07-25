942c65e01f7f503288ab3f70a0201bcaedd11d1e
Mauricio Ludwig <mrdamiengallagher@gmail.com>
2020-07-25 04:30:01 +0200

    refactor

--- src/problems/0007.ts
+++ src/problems/0007.ts
@@ -1,4 +1,5 @@
 import { success, end } from '../utils/logger';
+import { isPrimeNumber } from '../utils/math';
 
 export default () => {
   const primeNumbers = [2, 3, 5];
@@ -18,24 +19,4 @@ export default () => {
 
   success(`Answer: ${primeNumbers.pop()}`);
   end();
-};
-
-const isPrimeNumber = (n: number, primeNumbers: number[]) => {
-  const isDivisibleByTwo = n % 2 === 0;
-  const isDivisibleByFive = n % 5 === 0;
-  const isDivisibleByPrimeNumbers = primeNumbers.some((o) => n % o === 0);
-
-  const sumOfDigits = n
-    .toString()
-    .split('')
-    .map(Number)
-    .reduce((acc, curr) => acc + curr, 0);
-  const isDivisibleByThree = sumOfDigits % 3 === 0;
-
-  return !(
-    isDivisibleByTwo ||
-    isDivisibleByThree ||
-    isDivisibleByFive ||
-    isDivisibleByPrimeNumbers
-  );
 };
